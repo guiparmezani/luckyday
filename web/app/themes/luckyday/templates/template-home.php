a20745f89a33212b2b04ceff207a03b9bc01a838
Guilherme Parmezani <g.parmezani@gmail.com>
2016-04-27 22:21:02 -0500

    Home dev

---
+++ web/app/themes/luckyday/templates/template-home.php
@@ -0,0 +1,21 @@
+<?php
+/*
+* Template Name: Home Template
+*/
+?>
+
+<div class="main-banner">
+	<div class="container">
+		<div class="banner-content-wrapper">
+			<div class="banner-content">
+				<h1>Lucky Day</h1>
+				<p>Lorem ipsum dolor sit amet, ut wisi duis suspendisse quisque, varius in donec rutrum, morbi eu nihil posuere elementum id praesent</p>
+				<div class="buttons">
+					<a href="/try-the-demo" class="btn btn-brand">Try The Demo</a>
+					<a href="/buy-it-now" class="btn btn-brand">Buy It Now!</a>
+				</div>
+			</div>
+		</div>
+		<a href="#" class="down-angle"><i class="fa fa-angle-down" aria-hidden="true"></i></a>
+	</div>
+</div>
