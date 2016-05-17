120b914948ee6dae91a7497ef0507c8f182c9693
Guilherme Parmezani <g.parmezani@gmail.com>
2016-05-16 21:58:39 -0500

    Try out page dev

--- web/app/themes/luckyday/templates/template-try-out.php
+++ web/app/themes/luckyday/templates/template-try-out.php
@@ -4,4 +4,24 @@
 */
 ?>
 
-<?php get_template_part('templates/page-header'); ?>
+<?php while(have_posts()): the_post(); ?>
+	<?php get_template_part('templates/page-header'); ?>
+	<section class="section-template intro-text">
+		<div class="container">
+			<div class="intro-text-content">
+				<?php the_content(); ?>
+				<hr class="divider center">
+			</div>
+		</div>
+	</section>
+
+	<section class="try-out-section section-template">
+		<div class="container">
+			<div class="game-block">
+				<iframe src="/lucky-day-demo"></iframe>
+				<br><br>
+				<a href="/buy-it" class="btn btn-brand">Buy it now</a>
+			</div>
+		</div>
+	</section>
+<?php endwhile; ?>
