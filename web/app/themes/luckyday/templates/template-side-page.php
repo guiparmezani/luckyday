1d571104d47a698d1a789392c29daf800bbcdc36
Guilherme Parmezani <gparmezani@fahrenheitmarketing.com>
2016-06-06 16:42:51 -0500

    Activated PayPal payments

--- web/app/themes/luckyday/templates/template-side-page.php
+++ web/app/themes/luckyday/templates/template-side-page.php
@@ -165,9 +165,9 @@
 							<?php $i=0; while(have_rows('games', 'options')): the_row(); ?>
 								<div class="paypal-games game-<?php echo $i; ?>">
 									<?php if (pll_current_language() === 'en'): ?>
-										<?php echo do_shortcode( '[wp_paypal_payment_box email="' . get_field('paypal_email_address', 'options') . '" options="Game:0.01" button_text="Pay Now" new_window="1" return="' . untrailingslashit(get_bloginfo('url')) . '/buy-it?purchased=' . sanitize_title(get_sub_field('game_name')) . '&payment_complete=true"]' ); ?>
+										<?php echo do_shortcode( '[wp_paypal_payment_box email="' . get_field('paypal_email_address', 'options') . '" options="Game:' . str_replace(',', '.', get_sub_field('game_price')) . '" button_text="Pay Now" new_window="1" return="' . untrailingslashit(get_bloginfo('url')) . '/buy-it?purchased=' . sanitize_title(get_sub_field('game_name')) . '&payment_complete=true"]' ); ?>
 									<?php else: ?>
-										<?php echo do_shortcode( '[wp_paypal_payment_box email="' . get_field('paypal_email_address', 'options') . '" options="Game:0.01" button_text="Comprar" new_window="1" return="' . untrailingslashit(get_bloginfo('url')) . '/compre-o-jogo?purchased=' . sanitize_title(get_sub_field('game_name')) . '&payment_complete=true"]' ); ?>
+										<?php echo do_shortcode( '[wp_paypal_payment_box email="' . get_field('paypal_email_address', 'options') . '" options="Game:' . str_replace(',', '.', get_sub_field('game_price')) . '" button_text="Comprar" new_window="1" return="' . untrailingslashit(get_bloginfo('url')) . '/compre-o-jogo?purchased=' . sanitize_title(get_sub_field('game_name')) . '&payment_complete=true"]' ); ?>
 									<?php endif; ?>
 								</div>
 							<?php $i++; endwhile; ?>
